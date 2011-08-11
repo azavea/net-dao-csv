e6c909b9ded17c1cd796ccdff470ae30e651fe93
Justin Walgran <jwalgran@azavea.com>
2011-08-11 15:55:56 -0400

    Removed shared assembly attributes
    The parent solution now contains a ProductAssemblyInfo.cs
    file that contains the attributes common to all the
    Azavea.Open.DAO.* assemblies.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,11 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Azavea.Open.DAO.CSV")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Azavea")]
 [assembly: AssemblyProduct("Azavea.Open.DAO.CSV")]
-[assembly: AssemblyCopyright("Copyright © Azavea 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 // Configure log4net using the .config file
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
@@ -23,16 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b1a99361-9945-4c48-9288-e54cc08c8b37")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
